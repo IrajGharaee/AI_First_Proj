95d5b787ff1e22cc19b77a6821a1170be38fe805
Amin <Xporo720@gmail.com>
2022-11-28 06:33:34 -0800

    create successor def

---
+++ successor.py
@@ -0,0 +1,8 @@
+from model import State
+from
+
+def successor(state: State) -> list[State]:
+    loc_x, loc_y = State.robot.get_location()
+
+    for i in {(1,0),(-1,0),(0,1),(0,-1)} :
+        pass
