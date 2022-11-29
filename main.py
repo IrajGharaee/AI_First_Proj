ae6ef80c1fcad23b9efff31432c3687b9787497e
Amin <Xporo720@gmail.com>
2022-11-29 03:36:22 -0800

    DFS algorithm test

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ n, m = input('').split()
 matrix = [input().split() for i in range(int(n))]
 utils = Utils(matrix)
 s = Successor(utils)
-states = s.successor(utils.root)
-stack = states
+stack = [utils.root]
 state = stack.pop()
-while len(state.butters) > 0:
-    # print("s:",state.robot)
-    states = s.successor(utils.root)
-    stack = stack + states
+while True:
+    print(state)
+    stack+=s.successor(state)
+    if not stack or not state.butters:
+        break
     state = stack.pop()
+
