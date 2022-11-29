9ac881d5b0b8773dee262d1bbe341b242f43dea1
Amin <Xporo720@gmail.com>
2022-11-29 03:35:04 -0800

    successor function repeat state not considered

--- successor.py
+++ successor.py
@@ -1,7 +1,6 @@
 from model import State
 from utils import Utils
 
-
 class Successor:
     utils: Utils
     
@@ -11,40 +10,48 @@ class Successor:
     def successor(self,state: State) -> list[State]:
         states = []
         for move in {(1,0),(-1,0),(0,1),(0,-1)} :
-            if self.check_location_free(state,move):
-                # print(state)
-                new_state = State(states)
-                # print(new_state)
+            r,b = self.check_location_free(state,move)
+            if r:
+                new_state = State(state)
                 new_state.robot += move
-                states.append(new_state)
+                if b:
+                    _x, _y = state.robot.get_location()
+                    if self.utils.goals[_x][_y]:
+                        new_state.butters.remove(b)
+                        new_state.pass_butters.append(b+move)
+                    else:
+                        new_state.butters.remove(b)
+                        new_state.butters.append(b + move)
+                s = state.parent
+                repeat = False
+                while s:
+                    if s == new_state:
+                        repeat = True
+                        break
+                    s = s.parent
+                if not repeat:
+                    states.append(new_state)
         return states
     def check_location_free(self,state, move):
         _x, _y = state.robot.get_location()
         if 0 <= _x + move[0] < self.utils.n and 0 <= _y + move[1] < self.utils.m:
             if self.utils.blockages[ _x + move[0] ][ _y + move[1]]:
-                return False
+                return False,False
             for butter in state.butters:
                 if butter == [ _x + move[0] , _y + move[1] ]:
                     if 0 <= _x + 2*move[0] < self.utils.n and 0 <= _y + 2*move[1] < self.utils.m:
                         if self.utils.blockages[_x + 2*move[0]][_y + 2*move[1]]:
-                            return False
+                            return False,False
                         for b in state.butters:
                             if b == [ _x + 2*move[0] , _y + 2*move[1] ]:
-                                return False
+                                return False,False
+                        return True,butter
                     else:
-                        return False
+                        return False,False
             for butter in state.pass_butters:
                 if butter == [ _x + move[0] , _y + move[1] ]:
-                    return False
-            s = state.parent
-            while s:
-                # print(s)
-                # print(state)
-                # print(s==state)
-                if s == state:
-                    return False
-                s = s.parent
-            return True
+                    return False,False
+            return True,False
         else:
-            return False
+            return False,False
 
