9fc540fade2b565ac10bef4c73ad7ab15a1cf744
Amin <Xporo720@gmail.com>
2022-11-30 01:52:08 -0800

    DFS BFS IDS UCS function add

---
+++ algorithm.py
@@ -0,0 +1,46 @@
+def dfs(root,s):
+    stack = [root]
+    state_set = set([])
+    state = stack.pop()
+    state_set.add(state)
+    while stack or not state.butters:
+        stack += s.successor(state, state_set)
+        state = stack.pop()
+    return state
+
+def bfs(root,s):
+    stack = [root]
+    state_set = set([])
+    state = stack.pop(0)
+    state_set.add(state)
+    while stack or not state.butters:
+        stack += s.successor(state, state_set)
+        state = stack.pop(0)
+    return state
+
+def ids(root,s):
+    depth = 0
+    while True:
+        stack = [root]
+        state_set = set([])
+        state_set.add(root)
+        state = root
+        while stack and state.butters:
+            state = stack.pop()
+            if state.depth <= depth:
+                stack += s.successor(state, state_set)
+            if not state.butters:
+                return state
+        depth += 1
+        if depth > 100:
+            return None
+
+def ucs(root,s):
+    states = [root]
+    state_set = set([])
+    state_set.add(root)
+    state = root
+    while states and state.butters:
+        state = states.pop(states.index(max(states)))
+        states += s.successor(state, state_set)
+    return state
