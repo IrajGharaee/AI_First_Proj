4aa780a36461b93e48d58108ea32fae538f87724
Amin <Xporo720@gmail.com>
2022-12-02 01:22:30 -0800

    Test

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from successor import Successor
 from model import objects
 from algorithm import dfs,bfs,ucs,ids
 import os
+import time
+
+"""
+6 10
+1r 1 1 1 x x 1 1 1 1
+1 x 1 1 2 2 1 1 1 1
+x 1 1 2b 2 2 2b 1 x x
+x 1 1 x x 2 2 1 1p x
+1 1 1 1 2 2 1 1 1 1
+1 1 1 1 x 1p x 1 1 1
+"""
 
 clear = lambda: os.system('cls')
 n, m = input('').split()
@@ -10,13 +21,17 @@ matrix = [input().split() for i in range(int(n))]
 utils = Utils(matrix)
 s = Successor(utils)
 
-state_dfs = dfs(utils.root,s)
+# state_dfs = dfs(utils.root,s)
 state_bfs = bfs(utils.root,s)
-state_ids = ids(utils.root,s)
-state_ucs = ucs(utils.root,s)
+# state_ids = ids(utils.root,s)
+# state_ucs = ucs(utils.root,s)
 
-clear()
+state = state_bfs
+states = []
+while state:
+    states.append(state)
+    state = state.parent
 
-while state_ucs:
-    utils.print_matrix(state_ucs)
-    state_ucs = state_ucs.parent
+while states:
+    utils.print_matrix(states.pop())
+    time.sleep(2)
