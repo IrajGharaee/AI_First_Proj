789d3c86990fd998107bc328f095fc482992d024
Amin <Xporo720@gmail.com>
2022-12-10 00:24:57 -0800

    update main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from utils import Utils
 from successor import Successor
-from model import objects
-from algorithm import dfs,bfs,ucs,ids
-import os
-import time
+from algorithm import dfs,bfs,ucs,ids,best_first_search,A_star
+
 
 """
 6 10
@@ -15,23 +13,25 @@ x 1 1 x x 2 2 1 1p x
 1 1 1 1 x 1p x 1 1 1
 """
 
-clear = lambda: os.system('cls')
 n, m = input('').split()
 matrix = [input().split() for i in range(int(n))]
 utils = Utils(matrix)
 s = Successor(utils)
 
 # state_dfs = dfs(utils.root,s)
-# state_bfs = bfs(utils.root,s)
+state_bfs = bfs(utils.root,s)
 # state_ids = ids(utils.root,s)
-state_ucs = ucs(utils.root,s)
+# state_ucs = ucs(utils.root,s)
+# state_A_star = A_star(utils.root,s,utils)
+# state_best_first_search = best_first_search(utils.root,s,utils)
 
-state = state_ucs
+state = state_bfs
 states = []
 while state:
     states.append(state)
     state = state.parent
 
 while states:
-    utils.print_matrix(states.pop())
-    time.sleep(2)
+    print(states.pop().move,end="")
+    # utils.print_matrix(states.pop())
+    # time.sleep(2)
